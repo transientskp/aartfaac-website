2eec1f76e65d263003b0907460090f9e67c6c72c
Gijs Molenaar <gijs@pythonic.nl>
2016-11-01 12:42:06 +0100

    stream shizzle to youtube

--- monitoring/management/commands/listenforvis.py
+++ monitoring/management/commands/listenforvis.py
@@ -3,6 +3,10 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from arthur.writer import create_all_images
 from arthur.io import listen_socket
+from arthur.stream import setup_stream_pipe, stream
+
+
+logging.basicConfig()
 
 
 class Command(BaseCommand):
@@ -10,9 +14,18 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--port', nargs=1, type=int, default=5000)
+        parser.add_argument('--frequency', nargs=1, type=float, default=58398437.5)
 
     def handle(self, *args, **options):
+        port = options['port']
+        frequency = options['frequency']
+
         logger = logging.getLogger()
-        logger.setLevel(logging.INFO)
+        logger.setLevel(logging.DEBUG)
         logger.addHandler(logging.StreamHandler(self.stdout))
-        create_all_images([listen_socket(options['port'])], settings.MEDIA_ROOT)
+
+        pipe = setup_stream_pipe(settings.YOUTUBE_URL)
+
+        images = create_all_images(listen_socket(port), settings.MEDIA_ROOT,
+                                   frequency)
+        stream(images, pipe)
--- monitoring/management/commands/parsevisfile.py
+++ monitoring/management/commands/parsevisfile.py
@@ -1,5 +1,6 @@
 import logging
 from django.core.management.base import BaseCommand
+from itertools import chain
 from arthur.io import read_full
 from arthur.writer import create_all_images
 from django.conf import settings
@@ -10,10 +11,12 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('vis_file', nargs='+', type=str)
+        parser.add_argument('--frequency', nargs=1, type=float, default=58398437.5)
 
     def handle(self, *args, **options):
+        frequency = options['frequency']
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
         logger.addHandler(logging.StreamHandler(self.stdout))
-        create_all_images(map(read_full, options['vis_file']),
-                          settings.MEDIA_ROOT)
+        list(create_all_images(chain(*map(read_full, options['vis_file'])),
+                               settings.MEDIA_ROOT, frequency))
