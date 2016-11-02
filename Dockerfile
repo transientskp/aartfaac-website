8a3e108942ea6b43900a5deef2b7c13d8acf563d
Gijs Molenaar <gijs@pythonic.nl>
2016-11-02 16:55:57 +0100

    enable multiprocessing

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM kernsuite/base:1
 
-RUN docker-apt-install python3-casacore python3-pip
+# note: not sure if tk is required if we choose different matplotlib backend
+RUN docker-apt-install python3-casacore python3-pip python3-tk ffmpeg
 
 ADD requirements.txt /requirements.txt
 RUN pip3 install -r /requirements.txt
--- monitoring/management/commands/listenforvis.py
+++ monitoring/management/commands/listenforvis.py
@@ -1,12 +1,46 @@
 import logging
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from arthur.writer import create_all_images
+from arthur.writer import make_imaging_closure
 from arthur.io import listen_socket
+from arthur.imaging import full_calculation
 from arthur.stream import setup_stream_pipe, stream
-
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from multiprocessing import Manager
 
 logging.basicConfig()
+
+logger = logging.getLogger(__name__)
+
+
+def image_queue_pusher(date, body, frequency, queue):
+    result = full_calculation(body, frequency)
+    queue.put([date] + list(result))
+
+
+def queue_repeater(in_queue, out_queues):
+    logger.debug("repeater starting")
+    while True:
+        result = in_queue.get()
+        logger.debug("Got something from repeat queue!")
+        for queue in out_queues:
+            queue.put(result)
+
+
+def write_scheduler(queue, frequency):
+    imager_writer = make_imaging_closure(settings.MEDIA_ROOT, frequency)
+    while True:
+        logger.debug("recieved on writer queue")
+        result = queue.get()
+        imager_writer(*result)
+
+
+def stream_scheduler(queue):
+    pipe = setup_stream_pipe(settings.YOUTUBE_URL)
+    while True:
+        _, img_data, _, _ = queue.get()
+        logger.debug("Got something from stream queue!")
+        stream(img_data, pipe)
 
 
 class Command(BaseCommand):
@@ -14,7 +48,8 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--port', nargs=1, type=int, default=5000)
-        parser.add_argument('--frequency', nargs=1, type=float, default=58398437.5)
+        parser.add_argument('--frequency', nargs=1, type=float,
+                            default=58398437.5)
 
     def handle(self, *args, **options):
         port = options['port']
@@ -22,10 +57,22 @@ class Command(BaseCommand):
 
         logger = logging.getLogger()
         logger.setLevel(logging.DEBUG)
-        logger.addHandler(logging.StreamHandler(self.stdout))
+        handler =logging.StreamHandler(self.stdout)
+        handler.terminator = ""
+        fmt = logging.Formatter(fmt="%(asctime)s -- %(message)s")
+        handler.setFormatter(fmt)
+        logger.addHandler(handler)
 
-        pipe = setup_stream_pipe(settings.YOUTUBE_URL)
+        manager = Manager()
+        repeat_queue = manager.Queue()
+        writer_queue = manager.Queue()
+        stream_queue = manager.Queue()
 
-        images = create_all_images(listen_socket(port), settings.MEDIA_ROOT,
-                                   frequency)
-        stream(images, pipe)
+        with ProcessPoolExecutor() as executor:
+            executor.submit(queue_repeater, repeat_queue,
+                            [writer_queue, stream_queue])
+            executor.submit(write_scheduler, writer_queue, frequency)
+            executor.submit(stream_scheduler, stream_queue)
+            for date, body in listen_socket(port):
+                executor.submit(image_queue_pusher, date, body,
+                                frequency, repeat_queue)
